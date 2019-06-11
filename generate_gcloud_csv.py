79753043a18fe6d90a5d68b42cc799c1cd77f31a
Michael Trossbach <michotross257@gmail.com>
2019-06-11 12:43:07 -0400

    Capitalization formatting of 'CLASSES' variable

--- generate_gcloud_csv.py
+++ generate_gcloud_csv.py
@@ -8,7 +8,7 @@ from google.cloud import storage
 BUCKET_NAME    = '' # name of your gcloud bucket
 PROJECT_FOLDER = '' # name of project
 SAVE_PATH      = '' # path to CSV file. e.g. '/Users/johndoe/Desktop/gcloud_data.csv'
-classes        = [] # e.g. ['CLUSTER', 'DEEP', 'NEBULA', 'STARS']
+CLASSES        = [] # e.g. ['CLUSTER', 'DEEP', 'NEBULA', 'STARS']
 # --------------------------- EDIT THESE VALUES ---------------------------
 
 if __name__ == "__main__":
@@ -19,7 +19,7 @@ if __name__ == "__main__":
     with open(SAVE_PATH, "w") as file:
         csv_writer = csv.writer(file)
         for dataset in datasets:
-            for cls in classes:
+            for cls in CLASSES:
                 image_path = "{}/{}/{}/".format(PROJECT_FOLDER, dataset, cls)
                 for image in bucket.list_blobs(prefix=image_path):
                     csv_writer.writerow([dataset, "gs://" + bucket.name + "/" + image.name, cls])
