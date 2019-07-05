886e6184f076ee8ea28378c3caf234a1041f3c62
Michael Trossbach <michotross257@gmail.com>
2019-07-05 12:11:39 -0400

    Got rid of the file checking functionality in this Lambda

--- step-function/lambda-scripts/check_for_duplicate_upload/lambda_function.py
+++ step-function/lambda-scripts/check_for_duplicate_upload/lambda_function.py
@@ -6,43 +6,17 @@ table_id = os.environ.get('UPLOAD_HISTORY_TABLE')
 db_resource = boto3.resource('dynamodb')
 upload_history_table = db_resource.Table(table_id)
 
-class InvalidExtensionError(Exception):
-    pass
-
-
 
 def lambda_handler(event, context, call=None, callback=None):
-    response = event['detail']['requestParameters']
-    bucket = response['bucketName']
-    key = response['key']
-    
-    # image id is equal to the key minus the file extension, so get rid of extension
-    index = -1
-    while key[index] != '.':
-        index -= 1
-    image_id = key[:index]
-    extension = key[index:]
-    
-    msg = 'Expected .fits file extension but got {} file extension.'.format(extension)
-    if extension.lower() != '.fits':
-        raise InvalidExtensionError(msg)
-
     # to avoid duplicate uploads, check to see if image has been previously uploaded
     query = upload_history_table.query(
        ProjectionExpression="#img_id",
        ExpressionAttributeNames={ "#img_id": "IMAGE ID" },
        KeyConditionExpression="#img_id = :id",
-       ExpressionAttributeValues={":id": image_id}
+       ExpressionAttributeValues={":id": event['image_id']}
     )
 
-    print('\nQuery Result:', query['Items'])
     is_duplicate = True if len(query['Items']) else False
+    event['is_duplicate'] = is_duplicate
 
-    return {
-        'image_id': image_id,
-        'is_duplicate': is_duplicate,
-        's3': {
-            'key': key,
-            'bucket': bucket
-        }
-    }
+    return event
